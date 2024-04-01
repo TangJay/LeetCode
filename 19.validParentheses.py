9c70cb4ac9fdbbe560154c791718769878fdd253
Jay <jay723.tang@gmail.com>
2024-04-01 20:06:13 +0800

    new:19

---
+++ 19.validParentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == "(" or i == "[" or i == "{":
+                stack.append(i)
+            elif len(stack) == 0:
+                return False
+            elif i == ")":
+                if stack.pop() != "(":
+                    return False
+            elif i == "]":
+                if stack.pop() != "[":
+                    return False
+            elif i == "}":
+                if stack.pop() != "{":
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
